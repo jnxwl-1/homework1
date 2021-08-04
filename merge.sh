59f6541f3dc2e09f4ad8813b38a46a0a7c4bc01b
Евгений Кандауров <e@servicews.ru>
2021-08-04 21:23:46 +0300

    merge: use shift

--- merge.sh
+++ merge.sh
@@ -1,10 +1,9 @@
-  GNU nano 5.8                                  merge.sh
 #!/bin/bash
 # display command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+while [[ -n "$1" ]]; do
+    echo "Parameter #$count = $1"
     count=$(( $count + 1 ))
+    shift
 done
-
